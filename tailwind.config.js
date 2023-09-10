03b5677aa4a6e40a2c7cc9bb6d753d0993d0a037
makhti733@gmail.com <makhti733@gmail.com>
2023-09-10 22:50:39 +0330

    upper

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,18 @@
 module.exports = {
   content: ["./build/*.{html,js}"],
   theme: {
+      screens:{
+        'small':'388px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        'collection':'434px',
+      },
     extend: {
       colors:{
-        red: '#E71D36',
-        blue: '#02182b',
+        red: '#ed1c24',
+        blue: '#050517',
         gray:'#E4D9FF',
         kerem:'#F9F5E3',
         back:'rgba(228, 217, 255,0.7)'
@@ -15,7 +23,7 @@ module.exports = {
         boldfont:['Rowdies', 'cursive'],
         favboxfont:['Rubik Mono One', 'sans-serif'],
         boldfont2:['Titan One', 'cursive'],
-      }
+      },
     },
   },
   plugins: [],
